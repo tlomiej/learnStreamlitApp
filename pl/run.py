e400cbadfbf2960fb2f0010b121cc5cea5e9115d
tlomiej <bartekstaron@gmail.com>
2024-06-17 15:30:51 +0200

    add sample_data

--- pl/run.py
+++ pl/run.py
@@ -1,21 +1,20 @@
-# -*- coding: utf-8 -*-
-#######################
-# Import libraries
 import streamlit as st
 import pandas as pd
 import json
 #import altair as alt
 import plotly.express as px
 
-#######################
-# Page configuration
+
+if 'sample_data' not in st.session_state:
+    st.session_state.sample_data = False
+
+
 st.set_page_config(
-    page_title="Poland data",
+    page_title="Poland GUS data",
     page_icon=":seedling:",
     layout="wide",
     initial_sidebar_state="expanded")
 
-#alt.themes.enable("dark")
 
 
 #Data
@@ -25,27 +24,38 @@ with open('../data/woj_medium.geojson') as f:
 dfd = pd.read_csv('../data/test_data.csv', sep=';', encoding='utf-8', quotechar='"')
 
 
+
+def show_sample_data():
+    print('Klik')
+    st.session_state.sample_data = True
+
+
+    #dfd = pd.read_csv('../data/test_data.csv', sep=';', encoding='utf-8', quotechar='"')
+
+
+
 # Sidebar
 with st.sidebar:
+    st.title(':seedling: Poland GUS data')
     
-    st.title(':seedling: Poland data')
-    #selected_column_id = st.selectbox('Select a column id', list(dfd.columns))
-    selected_column_value = st.selectbox('Select a column value', list(dfd.columns), index=2)
-    if dfd[selected_column_value].isnull().any():
-        st.warning(f"There is no data. Select another column")
+
+    uploaded_file = st.sidebar.file_uploader("Choose a CSV file", type="csv")
 
 
+    # Wczytanie danych z pliku
+    if uploaded_file is not None or st.session_state.sample_data:
+        
+        df = pd.read_csv('../data/test_data.csv' if st.session_state.sample_data else uploaded_file)
 
-    color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
-    selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
+        selected_column_value = st.selectbox('Select a column value', list(dfd.columns), index=2)
+        if dfd[selected_column_value].isnull().any():
+            st.warning(f"There is no data. Select another column")
 
-
+        color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
+        selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
 
 #Map
-
 df = pd.DataFrame(dfd)
-
-
 
 def make_choropleth(input_df,input_color_theme, selected_column_id, selected_column_value):
     fig = px.choropleth(
@@ -79,15 +89,19 @@ def make_choropleth(input_df,input_color_theme, selected_column_id, selected_col
     return fig
 
 
+if uploaded_file is not None or st.session_state.sample_data:
+    st.markdown(f'#### {selected_column_value}')
+    choropleth = make_choropleth('df_selected_year', selected_color_theme,'selected_column_id', selected_column_value)
+    st.plotly_chart(choropleth, use_container_width=True)
+    col1, col2 = st.columns(2, gap='small')
+    col1.metric(label="Min", value=min(dfd[selected_column_value]))
+    col2.metric(label="Max", value=max(dfd[selected_column_value]))
+else:
+    st.markdown(f'''#### Download data from ''')
+    st.markdown(f"[Here](https://bdl.stat.gov.pl/bdl/dane/podgrup/temat)")
+    st.markdown("and load.")
 
-#col = st.columns((1.5, 5, 2), gap='medium')
-
-
-#with col[1]:
-st.markdown(f'#### {selected_column_value}')
-    
-choropleth = make_choropleth('df_selected_year', selected_color_theme,'selected_column_id', selected_column_value)
-st.plotly_chart(choropleth, use_container_width=True)
-    
+    st.button("Show sample data", on_click=show_sample_data)
+        
 
 

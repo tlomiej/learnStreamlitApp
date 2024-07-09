3dbcaa2ff116e4517e471e68b1e8d055d299f92f
tlomiej <bartekstaron@gmail.com>
2024-07-09 16:25:06 +0200

    Magnitude

--- earthquakes/run.py
+++ earthquakes/run.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 
 
@@ -9,6 +10,20 @@ st.markdown("Source data : https://www.kaggle.com/datasets/stealthtechnologies/e
 
 
 df = pd.read_csv('data/data.csv')
+
+
+# Magnitude
+min_magnitude = np.round(df['Magnitude'].min(), 1)
+max_magnitude = np.round(df['Magnitude'].max(), 1)
+start_magnitude, end_magnitude = st.select_slider(
+    "Magnitude",
+    options = np.round(np.arange(min_magnitude, max_magnitude + 0.1, 0.1), 1),
+    value=(min_magnitude,max_magnitude))
+
+
+st.write(start_magnitude)
+st.write(end_magnitude)
+
 
 st.dataframe(df)
 
